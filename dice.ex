8cc495e73770443a2c532bed25c224e2f436492e
NuTasha Fischer <nutashasemail@gmail.com>
2017-05-23 18:08:18 -0600

    Update dice.ex

--- dice.ex
+++ dice.ex
@@ -1,22 +1,18 @@
 defmodule Dice do
- IO.puts "This is a dice rolling game"
-
+ IO.puts "Welcome to The Dice Game"
   dice1 = ([1,2,3,4,5,6])
   val1 = Enum.random(dice1)
   dice2 = ([1,2,3,4,5,6])
   val2 = Enum.random(dice2)
   total = val1 + val2
-
-  IO.puts """ 
-          You rolled a #{val1}
-          and #{val2}.That gives you a total of total :#{total}
-          """
+       IO.puts "You rolled a #{val1} and #{val2}.That gives you a total of #{total}"
     cond do
        total == 7 ->
-        IO.puts "You Win."
-      total == 2 ->
-        IO.puts "Snake Eyes. You Lose."
-      true ->
-        IO.puts "It's a Draw."
+            IO.puts "You rolled a 7! You Win!"
+       total == 2 ->
+            IO.puts "Snake Eyes. You Lose."
+       true ->
+            IO.puts "It's a Draw. You must roll a total of 7 to win"
     end
+      IO.puts "Thank You For Playing"
 end
